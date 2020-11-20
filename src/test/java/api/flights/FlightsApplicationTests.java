da7e2e5a0ad5bb6c243a6408a3be26ac68474e99
Riday <ridayshah2001@gmail.com>
2020-11-20 17:41:52 +0530

    Feat: spring application main files
    Co-authored-by: Shivani Jha <shivaniijha99@gmail.com>

---
+++ src/test/java/api/flights/FlightsApplicationTests.java
@@ -0,0 +1,13 @@
+package api.flights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
